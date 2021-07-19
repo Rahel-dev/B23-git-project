06bd198862a8bcd7711d179d297433c00ec3dba0
Rahel <rahelasgedom19@gmail.com>
2021-07-19 16:20:39 -0400

    added some codes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello Git world");
+        System.out.println("Adding awesome codes here");
     }
 }
