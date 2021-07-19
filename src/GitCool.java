f21c797d0b6daeee23326668eaa34b1a5695492e
Rahel <rahelasgedom19@gmail.com>
2021-07-19 16:42:20 -0400

    Added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,7 @@ public class GitCool {
     //B23 is a very special number
     //B23 is making some sense now
 
+    // We have added our Github account under intellij
+    //make sure to click apply and click on to save
+
 }
